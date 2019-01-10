976661b504f30be9093bdf8dbabc031db9926979
Katie Blizzard <kblizzard11@gmail.com>
2019-01-10 00:12:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 function fetchBooks() {
  fetch("https://anapioficeandfire.com/api/books")
-  .then(function(response) {
-    response.json();
-  })
-  .then(function(json) {
-    console.log(json);
-  })
+  .then(resp => resp.json())
+  .then(json => console.log(json));
 }
 
 function renderBooks(json) {
